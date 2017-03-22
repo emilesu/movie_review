c7d954f65153f8bbaa0e9743871684ca5d247bb4
emilesu <emilesu@qq.com>
2017-03-22 23:12:17 +0800

    use before_action to find_movie_and_check_permission

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_movie_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @movies = Movie.all
@@ -24,11 +25,9 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:id])
     if @movie.update(movie_params)
       redirect_to movies_path, notice: "编辑电影成功！"
     else
@@ -37,8 +36,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
-
+    @movie.destroy
       redirect_to movies_path, notice: "已删除电影 - "+@movie.title
   end
 
@@ -49,5 +47,13 @@ class MoviesController < ApplicationController
 
   def movie_params
     params.require(:movie).permit(:title, :description)
+  end
+
+  def find_movie_and_check_permission
+    @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert: "你没有权限这样做！"
+    end
   end
 end
